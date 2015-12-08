59f35f13d267702d185babfd7a825a3999dc6a79
Christo Hughes <ChristoHughes@Christos-MacBook-Air.local>
2015-12-08 14:28:57 -0500

    TotalInventoryPage structure done

--- src/main/java/com/techelevator/toolLibrary/controller/NewLoanController.java
+++ src/main/java/com/techelevator/toolLibrary/controller/NewLoanController.java
@@ -3,6 +3,7 @@ package com.techelevator.toolLibrary.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,40 +13,47 @@ import com.techelevator.toolLibrary.model.LoanDAO;
 
 @Controller
 public class NewLoanController {
+	
+	private LoanDAO loanDAO;
+	
+	@Autowired
+	public NewLoanController(LoanDAO loanDAO) {
+		this.loanDAO = loanDAO;
+	}
 
-		@RequestMapping( path={"", "/", "/addTools"} )
+		@RequestMapping( path={"/addTools"} )
 		public String showInventoryList(Map <String, Object> model) {
-			List <Tool> toolList = LoanDAO.getListofAvailableTools();
+			List <Tool> toolList = loanDAO.getListofAvailableTools();
 			model.put("toolList", toolList);
 			return "inventoryList";  
 		}
 			
-		@RequestMapping( path={"", "/", "/addToCart"} )
+		@RequestMapping( path={"/addToCart"} )
 		public String addToCart(@RequestParam("toolId") int tool_id) {
 			List <Tool> shoppingCart = 
 			return "inventoryList";  
 		}
 		
 		
-		@RequestMapping( path={"", "/", "/viewToolLoan"} )
+		@RequestMapping( path={"/viewToolLoan"} )
 		public String viewLoan() {
 			return "loanCheckout";  
 		}
 		
 		
-		@RequestMapping( path={"", "/", "/checkoutTools"} )
+		@RequestMapping( path={"/checkoutTools"} )
 		public String processLoan() {
 			return "processLoan";  
 		}
 		
 		
-		@RequestMapping( path={"", "/", "/reviewLoan"} )
+		@RequestMapping( path={"/reviewLoan"} )
 		public String reviewLoan() {
 			return "reviewProcessedLoan";  
 		}
 
 		
-		@RequestMapping( path={"", "/", "/homePage"} )
+		@RequestMapping( path={"/homePage"} )
 		public String homePage() {
 			return "redirect:homePage"; 
 		}
--- src/main/webapp/WEB-INF/jsp/TotalInventoryPage.jsp
+++ src/main/webapp/WEB-INF/jsp/TotalInventoryPage.jsp
@@ -16,30 +16,32 @@
     
     <h2>Inventory</h2>
     
-    
-    
+          <c:url value="/addToCart" var="addToCartURL">  
+     	  <c:param name="tool_id" value="${tool.tool_id}" />
+     	  </c:url>
     <ul class="inventoryList">
         
         <c:forEach var="tool" items="${toolList}" varStatus="loopStatus">  
-       
             <div>
-            	${tool}
-            <c:url value="/addToCart" var="addToCartURL">
-			    <c:param name="tool_id" value="${tool.tool_id}" />    
-                <img src="/webapp/img/shopping_cart.png"/></a>      <!--http request surrounds image, to make shopping cart a button -->
-            </c:url>
-            
-
+            	 ${tool}          	
+     				<a href ="${addToCartURL}">
+                		<img src="/webapp/img/shopping_cart.png">
+                 	</a>     								
                    <hr>
             </div>
         </c:forEach>
-        
     </ul>
     
-    <c:url value="/forumTopicInput" var="topicInputURL" />
-    [ <a href="${topicInputURL}">Add a Topic</a> ]
-
-    <jsp:include page="/WEB-INF/jsp/footer.jsp" />
+    <c:url value="/viewToolLoan" var="loanCheckout" />
+    
+  				<div>
+     				<a href ="${loanCheckout}">
+                		<img src="/webapp/img/shopping_cart.png">
+                 	</a>    
+                 </div>
+                 
+                 
+  			<!--  <jsp:include page="/WEB-INF/jsp/footer.jsp" />    -->     
 </body>
 
 </html>
