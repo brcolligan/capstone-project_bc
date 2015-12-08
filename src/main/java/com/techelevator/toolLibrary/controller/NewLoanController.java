c9cfe2a59a2291abeb2852a073e0e532d324ad95
Christo Hughes <ChristoHughes@Christos-MacBook-Air.local>
2015-12-08 13:33:05 -0500

    with Controller saved this time

--- src/main/java/com/techelevator/toolLibrary/controller/NewLoanController.java
+++ src/main/java/com/techelevator/toolLibrary/controller/NewLoanController.java
@@ -1,16 +1,31 @@
 package com.techelevator.toolLibrary.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.techelevator.Tool;
+import com.techelevator.toolLibrary.model.LoanDAO;
 
 @Controller
 public class NewLoanController {
 
 		@RequestMapping( path={"", "/", "/addTools"} )
-		public String showInventoryList() {	
+		public String showInventoryList(Map <String, Object> model) {
+			List <Tool> toolList = LoanDAO.getListofAvailableTools();
+			model.put("toolList", toolList);
 			return "inventoryList";  
 		}
 			
+		@RequestMapping( path={"", "/", "/addToCart"} )
+		public String addToCart(@RequestParam("toolId") int tool_id) {
+			List <Tool> shoppingCart = 
+			return "inventoryList";  
+		}
+		
 		
 		@RequestMapping( path={"", "/", "/viewToolLoan"} )
 		public String viewLoan() {
