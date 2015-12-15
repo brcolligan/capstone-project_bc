34d935b2330061dd0fd243fef306a4b48f0d2c23
Christo Hughes <ChristoHughes@Christos-MacBook-Air.local>
2015-12-15 10:04:27 -0500

    calculateFees finished, and NewLoanController update

--- src/main/java/com/techelevator/toolLibrary/model/FeesCalculator.java
+++ src/main/java/com/techelevator/toolLibrary/model/FeesCalculator.java
@@ -9,30 +9,45 @@ public class FeesCalculator {
 	private Tool tool;
 	private LocalDate expectedReturn;
 	private LocalDate endDate;
-	static final double handToolDailyLateFee = 0.50d;
-	static final double powerToolDailyLateFee = 1.00d;
-	static final double dayRentalDailyLateFee = 5.0d;
-	static final double gasPoweredMaintFee = 2.0d;
-	static final double cleaningFee = 0.0d; // librarian discretion.
-	double totalFees = 0d;
+	private static final double handToolDailyLateFee = 0.50d;
+	private static final double powerToolDailyLateFee = 1.00d;
+	private static final double dayRentalDailyLateFee = 5.0d;
+	private static final double gasPoweredMaintFee = 2.0d;
+	private double cleaningFee = 0.0d; // librarian discretion.
+	double lateFees = 0.0d;
 
-	public double calculateFees(LocalDate expectedReturn, LocalDate endDate, double cleaningFee) {
+	public double calculateCleaningFee(boolean applyCleaningFee) {
+		if (applyCleaningFee) {
+			return 5.0;
+		} else {
+			return 0.0;
+		}
+	}
+
+	public double calculateMaintenanceFees(String toolCategoryName) {
+		if (toolCategoryName == "Gasoline Power Tool") {
+			return gasPoweredMaintFee;
+		} else {
+			return 0.0;
+		}
+	}
+
+	public double calculateLateFees(LocalDate expectedReturn, LocalDate endDate) {
 		this.expectedReturn = expectedReturn;
 		this.endDate = endDate;
 		long daysLate = DAYS.between(expectedReturn, endDate);
 
-		if (daysLate > 0) {
-			if (tool.getToolCategoryName() == "Electric Power Tool") {
-				totalFees = (daysLate * powerToolDailyLateFee) + cleaningFee;
-			} else if (tool.getToolCategoryName() == "Gasoline Power Tool") {
-				totalFees = (daysLate * powerToolDailyLateFee) + gasPoweredMaintFee + cleaningFee;
-			} else if (tool.getToolCategoryName() == "Hand Tool") {
-				totalFees = (daysLate * handToolDailyLateFee) + cleaningFee;
-			}
-		} else {
-			totalFees = 0 + cleaningFee;
+		if (daysLate > 0 && tool.getToolLoanPeriod() == 1) {
+			lateFees += daysLate * dayRentalDailyLateFee;
 		}
 
-		return totalFees;
+		if (daysLate > 0) {
+			if (tool.getToolCategoryName() == "Electric Power Tool"||tool.getToolCategoryName()=="Gasoline Power Tool") {
+				lateFees += (daysLate * powerToolDailyLateFee);
+			} else if (tool.getToolCategoryName() == "Hand Tool") {
+				lateFees += (daysLate * handToolDailyLateFee);
+			}
+		} return lateFees;
 	}
+
 }
